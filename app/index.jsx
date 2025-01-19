2bd90cdc545c5e1911d25359fe7443bbfe12cb8d
Bohdan Khvorostovskyi <bohdankhv@gmail.com>
2025-01-19 11:34:03 -0500

    Add email login functionality and improve UI layout

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,16 @@
 import { useOAuth } from "@clerk/clerk-expo";
-import { WebBrowser } from "expo-web-browser";
-import { Text, View } from "react-native";
+import * as WebBrowser from 'expo-web-browser';
+import { Ionicons } from "@expo/vector-icons";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Colors } from "@/contansts/Colors";
 
 export default function Index() {
   const { startOAuthFlow } = useOAuth({ strategy: "oauth_apple" });
   const { startOAuthFlow: googleOAuth } = useOAuth({ strategy: "oauth_google" });
+  const { startOAuthFlow: emailOAuth } = useOAuth({ strategy: "email" });
+
+  const { top } = useSafeAreaInsets();
   
   const handleAppleLogin = async () => {
     try {
@@ -32,21 +38,93 @@ export default function Index() {
     }
   }
 
+  const handleEmailLogin = async () => {
+    try {
+      const { createSessionId, setActive } = await emailOAuth();
+      console.log(`Email login: ${createSessionId}`);
+
+      if (createSessionId) {
+        setActive({ session: createSessionId });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   const openLink = async (url) => {
-    WebBrowser.openBrowserAsync(url);
+    await WebBrowser.openBrowserAsync(url);
   }
 
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={[styles.container, { paddingTop: top }]}
     >
-      <Text>
-        Hi there!
-      </Text>
+      <Image
+        source={require("@/assets/images/todoist-logo.png")}
+        style={styles.logoImage}
+      />
+      <Image
+        source={require("@/assets/images/login.png")}
+        style={styles.loginImage}
+      />
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity onPress={handleAppleLogin} style={styles.button}>
+          <Ionicons name="logo-apple" size={24} color="#000" />
+          <Text style={styles.buttonText}>Continue with Apple</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleGoogleLogin} style={[styles.button]}>
+          <Ionicons name="logo-google" size={24} color="#000" />
+          <Text style={styles.buttonText}>Continue with Google</Text>
+        </TouchableOpacity>
+        {/* <TouchableOpacity onPress={handleEmailLogin} style={[styles.button, { backgroundColor: "#fff", borderColor: "#000" }]}>
+          <Ionicons name="mail-outline" size={24} color="#000" />
+          <Text style={[styles.buttonText, { color: "#000" }]}>Sign in with Email</Text>
+        </TouchableOpacity> */}
+        <Text style={{ textAlign: "center", fontSize: 14, color: "#000" }}>
+          By signing in, you agree to our <Text style={{ color: Colors.primary }} onPress={() => openLink("https://todoist.com/terms")}>Terms of Service</Text> and <Text style={{ color: Colors.primary }} onPress={() => openLink("https://todoist.com/privacy")}>Privacy Policy</Text>
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 40,
+    marginTop: 30
+  },
+  logoImage: {
+    height: 50,
+    resizeMode: "contain",
+    alignSelf: "center"
+  },
+  loginImage: {
+    height: 350,
+    resizeMode: "contain",
+    alignSelf: "center"
+  },
+  buttonsContainer: {
+    gap: 14,
+    alignItems: "center",
+    paddingHorizontal: 40
+  },
+  button: {
+    flexDirection: "row",
+    gap: 10,
+    justifyContent: "center",
+    width: "100%",
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderColor: Colors.borderColor,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderRadius: 10,
+    alignItems: "center"
+  },
+  buttonText: {
+    fontWeight: "600",
+    fontSize: 16,
+    color: "#000"
+  },
+});
